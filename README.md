ea586e8d7d3757b9d5af72e14e57b0b6425492bf
Abhishek Karanath <akaranath@ee.iitr.ac.in>
2020-04-23 10:31:42 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Dual-Stage Attention-Based Recurrent Neural Net for Time Series Prediction based off [this blog post](https://chandlerzuo.github.io/about/) by Chandler Zuo.
+
+Minor changes to code taken from https://github.com/Seanny123/da-rnn
+
+Pytorch 1.4.0
+Python 3.8.1
---
+++ utils.py
@@ -0,0 +1,28 @@
+import logging
+import os, shutil
+
+import matplotlib.pyplot as plt
+import torch
+
+# device = torch.device("cuda:3" if torch.cuda.is_available() else "cpu")
+
+def create_exp_dir(path, scripts_to_save=None):
+    if not os.path.exists(path):
+        os.mkdir(path)
+
+    print('Experiment dir : {}'.format(path))
+    if scripts_to_save is not None:
+        os.mkdir(os.path.join(path, 'scripts'))
+        for script in scripts_to_save:
+            dst_file = os.path.join(path, 'scripts', os.path.basename(script))
+            shutil.copyfile(script, dst_file)
+
+def save_or_show_plot(file_nm: str, save: bool):
+    if save:
+        plt.savefig(os.path.join(os.path.dirname(__file__), "plots", file_nm))
+    else:
+        plt.show()
+
+
+def numpy_to_tvar(x):
+    return torch.from_numpy(x).type(torch.FloatTensor).cuda()
